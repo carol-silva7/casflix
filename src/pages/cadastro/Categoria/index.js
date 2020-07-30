7cedf606509457df3a60c021015e135b87c357b0
unknown <cah.silva7@gmail.com>
2020-07-30 17:11:49 -0300

    inclusão de useState

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const [nomeDaCategoria, setNomeDaCategoria] = useState(`Valor inicial`);
+
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
       <form>
+        {/* State*/}
         <label>
           Nome da Categoria:
-          <input type="text" />
+          <input type="text"
+            value={nomeDaCategoria}
+            onChange={function funcaoHandlerQueOErroPediu(infosDoEvento) {
+              console.log([nomeDaCategoria], nomeDaCategoria);
+              console.log([infosDoEvento.target.value], infosDoEvento.target.value)
+              setNomeDaCategoria(infosDoEvento.target.value);
+
+            }} />
         </label>
 
         <button>Cadastrar</button>
