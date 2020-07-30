22451a8c0b2e60fd82516d76e525b72a912f26dc
unknown <cah.silva7@gmail.com>
2020-07-30 01:01:51 -0300

    inclusão - router

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo-casflix.png';
 import './Menu.css';
 // import ButtonLink from './components/ButtonLink';
@@ -7,11 +8,11 @@ import Button from '../Button';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="Logo Casflix" />
-      </a>
-      <Button as="a" className="ButtonLink" href="/" alt="botÃ£o">
-        Novo vÃ­deo
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video" alt="botão">
+        Novo vídeo
       </Button>
     </nav>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from '../src/components/Menu';
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: '#141414' }}>
       <Menu />
@@ -14,7 +14,7 @@ function App() {
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={
-          'O que Ã© Front-end? Trabalhando na Ã¡rea os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!'
+          'O que é Front-end? Trabalhando na Área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!'
         }
       />
 
@@ -35,4 +35,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
